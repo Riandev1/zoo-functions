a383ba075902b3ccb2917ebf8f36c4d2825d52cb
oDevSilva <ocontato.dev@gmail.com>
2023-06-19 18:13:00 -0300

    Concluido exercicio 5, 6 e 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,54 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('getOpeningHours', () => {
+  const closedMessage = 'The zoo is closed';
+  const openMessage = 'The zoo is open';
+
+  it('deve retornar o objeto de horários de funcionamento quando nenhum argumento é fornecido', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+
+  it('deve retornar "The zoo is closed" para Monday e 09:00-AM', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe(closedMessage);
+  });
+
+  it('deve retornar "The zoo is open" para Tuesday e 09:00-AM', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe(openMessage);
+  });
+
+  it('deve retornar "The zoo is closed" para Wednesday e 09:00-PM', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe(closedMessage);
+  });
+
+  it('deve retornar "The zoo is open" para Saturday e 10:00-AM', () => {
+    expect(getOpeningHours('Saturday', '10:00-AM')).toBe(openMessage);
+  });
+
+  it('deve retornar "The zoo is closed" para Sunday e 07:00-PM', () => {
+    expect(getOpeningHours('Sunday', '07:00-PM')).toBe(openMessage);
+  });
+
+  it('deve retornar "The zoo is open" para Thursday e 11:00-AM', () => {
+    expect(getOpeningHours('Thursday', '11:00-AM')).toBe(openMessage);
+  });
+
+  it('deve lançar uma exceção para minutos inválidos', () => {
+    expect(() => (getOpeningHours('Tuesday', '09:60-AM'))).toThrow('The minutes must be between 0 and 59');
+  });
+
+  it('deve lançar uma exceção para minutos em formato inválido', () => {
+    expect(() => (getOpeningHours('Sunday', '09:c0-AM'))).toThrow('The minutes should represent a number');
+  });
+
+  it('deve lançar uma exceção para horas inválidas', () => {
+    expect(() => (getOpeningHours('Monday', '13:00-AM'))).toThrow('The hour must be between 0 and 12');
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,46 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('handlerElephants', () => {
+  const COUNT_ARGUMENT = 'count';
+  const NAMES_ARGUMENT = 'names';
+  const AVERAGE_AGE_ARGUMENT = 'averageAge';
+  const LOCATION_ARGUMENT = 'location';
+  const POPULARITY_ARGUMENT = 'popularity';
+  const AVAILABILITY_ARGUMENT = 'availability';
+
+  it('deve retornar o número inteiro 4 para o argumento count', () => {
+    const result = handlerElephants(COUNT_ARGUMENT);
+    expect(result).toBe(4);
+  });
+
+  it('deve retornar um array de nomes que inclui o nome "Jefferson" para o argumento names', () => {
+    const result = handlerElephants(NAMES_ARGUMENT);
+    expect(result).toContain('Jefferson');
+  });
+
+  it('deve retornar um número próximo a 10.5 para o argumento averageAge', () => {
+    const result = handlerElephants(AVERAGE_AGE_ARGUMENT);
+    expect(result).toBeCloseTo(10.5);
+  });
+
+  it('deve retornar a string "NW" para o argumento location', () => {
+    const result = handlerElephants(LOCATION_ARGUMENT);
+    expect(result).toBe('NW');
+  });
+
+  it('deve retornar um número igual ou maior a 5 para o argumento popularity', () => {
+    const result = handlerElephants(POPULARITY_ARGUMENT);
+    expect(result).toBeGreaterThanOrEqual(5);
+  });
+
+  it('deve retornar um array de dias da semana que não contém "Monday" para o argumento availability', () => {
+    const result = handlerElephants(AVAILABILITY_ARGUMENT);
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.includes('Monday')).toBe(false);
+  });
+
+  it('deve retornar undefined quando nenhum argumento é fornecido', () => {
+    const result = handlerElephants();
+    expect(result).toBeUndefined();
+  });
+});
