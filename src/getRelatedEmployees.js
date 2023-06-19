09d3fb1382ffffe1b974cfcda1c680d0c2504a9a
oDevSilva <ocontato.dev@gmail.com>
2023-06-19 16:30:58 -0300

    Concluido o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) =>
+  data.employees.some((emp) => emp.managers.find((empi) => empi === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const manager = data.employees.find((emp) => emp.id === managerId);
+
+  if (!manager || manager.managers.length !== 0)
+    throw new Error("O id inserido não é de uma pessoa colaboradora gerente!");
+
+  return data.employees
+    .filter((emp) => emp.managers.includes(managerId))
+    .map((emp) => `${emp.firstName} ${emp.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
