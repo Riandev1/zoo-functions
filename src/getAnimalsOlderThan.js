c67a9c994a9dbfd50286ef33be85e993ff58c5e0
oDevSilva <ocontato.dev@gmail.com>
2023-06-19 15:20:12 -0300

    Concluido o 2 requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+function getAnimalsOlderThan(especie, idadeMinima) {
+  const animalDaEspecie = data.species.find(animal => animal.name === especie);
+  
+  if (!animalDaEspecie) {
+    return false;
+  }
+  
+  return animalDaEspecie.residents.every(animal => animal.age >= idadeMinima);
+}
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
+const especie = 'lions';
+const idadeMinima = 10;
+
+const resultado = getAnimalsOlderThan(especie, idadeMinima);
+console.log(resultado); 
 
 module.exports = getAnimalsOlderThan;
