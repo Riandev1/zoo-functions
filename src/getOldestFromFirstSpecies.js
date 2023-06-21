54c7f8e31c3f114ca41f93bd128feb9b758f4e85
oDevSilva <ocontato.dev@gmail.com>
2023-06-21 00:08:05 -0300

    Feito o requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const responsibleForFirstAnimal = data.employees
+  .find((employee) => employee.id === id).responsibleFor[0];
+const filterasSpecie = data.species
+  .find((specie) => specie.id === responsibleForFirstAnimal).residents;
+return Object.values(filterasSpecie.sort((a, b) => a.age - b.age)[filterasSpecie.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
