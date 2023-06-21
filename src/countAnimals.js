5c89f73039efc0f331714550233195fe48e0926b
oDevSilva <ocontato.dev@gmail.com>
2023-06-20 23:37:24 -0300

    Feito o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const animalCount = {};
+  data.species.forEach((species) => {
+    animalCount[species.name] = species.residents.length;
+  });
+
+  return animal
+    ? data.species.find((species) => species.name === animal.species)
+      .residents.filter((resident) => (animal.sex ? resident.sex === animal.sex : true)).length
+    : animalCount;
 };
 
 module.exports = countAnimals;
