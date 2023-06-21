ab0c7897572f6ac587619496a69eba75715134cc
oDevSilva <ocontato.dev@gmail.com>
2023-06-21 00:10:21 -0300

    Corrigido os erros de lint, do requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,10 +2,10 @@ const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
   const responsibleForFirstAnimal = data.employees
-  .find((employee) => employee.id === id).responsibleFor[0];
-const filterasSpecie = data.species
-  .find((specie) => specie.id === responsibleForFirstAnimal).residents;
-return Object.values(filterasSpecie.sort((a, b) => a.age - b.age)[filterasSpecie.length - 1]);
+    .find((employee) => employee.id === id).responsibleFor[0];
+  const filterasSpecie = data.species
+    .find((specie) => specie.id === responsibleForFirstAnimal).residents;
+  return Object.values(filterasSpecie.sort((a, b) => a.age - b.age)[filterasSpecie.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
